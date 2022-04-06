99217d51dbf14b7af88c1e7f446e127b26d4494a
Litlfoot <100711572+Litlfoot@users.noreply.github.com>
2022-04-07 01:35:47 +0800

    Create home-page.php
    Home page editable for client

---
+++ home-page.php
@@ -0,0 +1,66 @@
+<?php
+    /*
+    Template Name: Home page
+    */
+    ?><?php get_header();?>   <!--  Tells WordPress to include header.php -->
+    <Section class="container-fluid aboutbg text-center">
+        <div class="container"><h2><?php the_field('titleforaboutme'); ?></h2><!--  custom field for the aboutme content-->
+            <div class="blocker"></div>
+        </div> <!-- container -->
+    </Section>
+    <section class="container-fluid articlesbg">
+        <div class=":container">
+            <h3 class="text-center">LATEST ARTICLES</h3>
+            <div class="row">
+                <div class="col-md-4">
+                    <img class="img-fluid" src="http://206.189.45.97/~mesh17/wp-content/themes/starter_for_josh_site/images/port1.jpg" alt="woman">
+                    <h4 class="article-title">A PORTRAIT</h4>
+                    <p class="date">12 December 2020</p>
+                    <p class="article-p">STS has never been shy of seeking new terrains of investigation. More
+                        and more
+                        STS scholars are
+                        starting to explore and intervene in the arts. This object of study brings new
+                        challenges and
+                        opportunities that we want to explore in this session. We would like to gather first of
+                        all
+                        simply new kinds of knowledge arising from STS study of the arts.
+                    </p>
+                    <a class="readmore" href="#">CONTINUE READING</a>
+                </div>
+
+                <div class="col-md-4">
+                    <img class="img-fluid" src="http://206.189.45.97/~mesh17/wp-content/themes/starter_for_josh_site/images/port2.jpg" alt="woman">
+                    <h4 class="article-title">A PORTRAIT</h4>
+                    <p class="date">12 December 2020</p>
+                    <p class="article-p">STS has never been shy of seeking new terrains of investigation. More
+                        and more
+                        STS scholars are
+                        starting to explore and intervene in the arts. This object of study brings new
+                        challenges and
+                        opportunities that we want to explore in this session. We would like to gather first of
+                        all
+                        simply new kinds of knowledge arising from STS study of the arts.
+                    </p><a class="readmore" href="#">CONTINUE READING</a>
+                </div>
+
+                <div class="col-md-4">
+                    <img class="img-fluid" src="http://206.189.45.97/~mesh17/wp-content/themes/starter_for_josh_site/images/port3.jpg " alt="woman">
+                    <h4 class="article-title">A PORTRAIT</h4>
+                    <p class="date">12 December 2020</p>
+                    <p class="article-p">STS has never been shy of seeking new terrains of investigation. More
+                        and more
+                        STS scholars are
+                        starting to explore and intervene in the arts. This object of study brings new
+                        challenges and
+                        opportunities that we want to explore in this session. We would like to gather first of
+                        all
+                        simply new kinds of knowledge arising from STS study of the arts.
+                    </p><a class="readmore" href="#">CONTINUE READING</a>
+                </div>
+
+            </div> <!-- row -->
+
+        </div> <!-- container -->
+    </section><?php get_footer();?>   <!-- Tells WordPress to include footer.php   -->
+
+   
