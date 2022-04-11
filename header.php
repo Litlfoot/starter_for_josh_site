8d74a60eb40ee3f5d47e3f33fb54f11c59c0a974
Litlfoot <100711572+Litlfoot@users.noreply.github.com>
2022-04-12 01:12:10 +0800

    Update header.php
    Still trying to find error

--- header.php
+++ header.php
@@ -4,17 +4,11 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>"><meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"> -->
-
-
+   
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@800&family=Yeseva+One&display=swap"
         rel="stylesheet">
-
-        <link rel="stylesheet" href="style.css">
-<link rel="stylesheet" href="responsive.css" />
 
     <title><?php
         if (function_exists('is_tag') && is_tag()) {
