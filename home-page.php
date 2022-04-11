1d9841c0b379911e8ff1f7ed38fcc6c02aab635d
Litlfoot <100711572+Litlfoot@users.noreply.github.com>
2022-04-11 21:40:47 +0800

    Update home-page.php
    Trying to figure out why header is not showing on homepage

--- home-page.php
+++ home-page.php
@@ -2,7 +2,7 @@
     /*
     Template Name: Home page
     */
-    ?><?php get_header();?>   <!--  Tells WordPress to include header.php -->
+    ?><?php get_header();?><!--  Tells WordPress to include header.php -->
     <Section class="container-fluid aboutbg text-center">
         <div class="container"><h2><?php the_field('titleforaboutme'); ?></h2><!--  custom field for the aboutme content--><p class="particle"><?php the_field('subtitleforaboutme'); ?></p>
 <p class="about-text"><?php the_field('paragraphforaboutme'); ?></p><div class="blocker"></div>
