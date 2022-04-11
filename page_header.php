306e223139fe72d5149efe9620578ac6ceb76215
Litlfoot <100711572+Litlfoot@users.noreply.github.com>
2022-04-11 22:38:57 +0800

    Update page_header.php
    Trying to figure out why header is not showing on homepage

--- page_header.php
+++ page_header.php
@@ -37,7 +37,7 @@
 
 <body>
 
-<header class="container-fluid" style="background-size:cover; height:40vh; background-image: url(<?php the_field('projectheaderbackgroundimage') ?>);"><div class="container">
+<header class="container-fluid smallpage-bg1" style="background-size:cover; height:40vh; background-image: url(<?php the_field('projectheaderbackgroundimage') ?>);"><div class="container">
 
 
             <nav class="navbar navbar-expand-md navbar-dark">
@@ -70,10 +70,8 @@
             </nav>
 
 
-            <div class="smallpage-tag">
-                <h1 class="smallpage-title" >Projects</h1>
-            </div>
-        </div> <!-- container --><?php wp_head(); ?>
+            <div class="smallpage-tag"><!-- div that holds the content in the middle of the page-->
+<h1 class="smallpage-title"><?php the_field('projectsheadertitle'); ?></h1></div><!-- container--><?php wp_head(); ?>
         <!-- very important that this is added-->
     </header>
 
