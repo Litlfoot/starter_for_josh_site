461b7695f2f1f5d2e85c8ca072daf1f712bfa467
Litlfoot <100711572+Litlfoot@users.noreply.github.com>
2022-04-12 02:00:05 +0800

    Update footer.php
    Making footer editable

--- footer.php
+++ footer.php
@@ -1,14 +1,14 @@
 <footer class="container-fluid text-center">
         <div class="container">
             <div class="row">
-                <div class="col-md-6">
-                    <a class="navbar-brand" href="#"><span class="josh">JOSH</span> <span class="orange">
+                <!-- <div class="col-md-6"> -->
+                    <!-- <a class="navbar-brand" href="#"><span class="josh">JOSH</span> <span class="orange">
                             WHITKIN</span>
-                    </a>
-                </div>
+                    </a> --><div class="col-md-6 widgetleft"><?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-left') ) : 
+endif; ?></div>
                 <div class="col-md-6">
-                    <p> Terms and Conditions </p>
-                </div>
+                    <!-- <p> Terms and Conditions </p> --><?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-right') ) : 
+endif; ?></div>
 
             </div> <!-- row -->
         </div> <!-- container -->
